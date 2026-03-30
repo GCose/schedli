2bdd760bfbd77523e209bf84f1507cf28b9465b5
GCose <goodnessadewuyi8@gmail.com>
2026-03-30 09:54:08 +0000

    Made 2 auth bug fixes

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import { Suspense } from "react";
 import SignInClient from "./SignInClient";
 
 export const metadata: Metadata = {
@@ -6,5 +7,9 @@ export const metadata: Metadata = {
 };
 
 export default function SignInPage() {
-  return <SignInClient />;
+  return (
+    <Suspense>
+      <SignInClient />
+    </Suspense>
+  );
 }
